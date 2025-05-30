a4e8afea0f8627644d4b43a7807eec6a347cd1ba
dongjunyun <ydj9398@naver.com>
2025-05-30 17:11:23 +0900

    cache delete

--- babyassist/static/sw.js
+++ babyassist/static/sw.js
@@ -1,5 +1,4 @@
-
-const CACHE_NAME = 'baby-video-player-v1';
+const CACHE_NAME = 'baby-video-player-v2';
 const urlsToCache = [
   '/',
   '/static/style.css',
@@ -9,18 +8,55 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        console.log('Opened cache:', CACHE_NAME);
+        return cache.addAll(urlsToCache);
+      })
   );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        // 캐시에 있으면 캐시에서 반환, 없으면 네트워크에서 가져오기
-        return response || fetch(event.request);
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then(
+          function(response_1) {
+            if(!response_1 || response_1.status !== 200 || response_1.type !== 'basic') {
+              return response_1;
+            }
+
+            var responseToCache = response_1.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response_1;
+          }
+        );
       }
     )
   );
